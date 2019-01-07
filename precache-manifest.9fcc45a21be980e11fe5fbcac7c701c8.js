92f699012b0c3f61c71b8106fceafde26e268876
arseniyT <30984464+arseniyT@users.noreply.github.com>
2019-01-07 21:25:15 +0300

    Updates

---
+++ precache-manifest.9fcc45a21be980e11fe5fbcac7c701c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd446185296733782980",
+    "url": "/hw_react-wizard/static/js/runtime~main.cd446185.js"
+  },
+  {
+    "revision": "2255a03a7d19636ad817",
+    "url": "/hw_react-wizard/static/js/main.2255a03a.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/hw_react-wizard/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "2255a03a7d19636ad817",
+    "url": "/hw_react-wizard/static/css/main.708c22f2.chunk.css"
+  },
+  {
+    "revision": "776caefc475e4b8d2dfff3831aa3a57a",
+    "url": "/hw_react-wizard/index.html"
+  }
+];
